45ea2d862ca4c61f0d97696b2ddc7f7062576bb5
YuGyeong98 <kuk3097@naver.com>
2024-05-14 17:22:22 +0900

    Feat: hello get 구현

---
+++ src/main/kotlin/hello/hellospringkotlin/controller/HelloController.kt
@@ -0,0 +1,15 @@
+package hello.hellospringkotlin.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HelloController {
+
+    @GetMapping("hello")
+    fun hello(model: Model): String {
+        model.addAttribute("data", "spring!")
+        return "hello"
+    }
+}
