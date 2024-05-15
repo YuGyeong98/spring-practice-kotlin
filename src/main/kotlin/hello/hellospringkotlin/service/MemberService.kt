ea1a5cc221334121f2626e2bb2baef5482d3cc2a
YuGyeong98 <kuk3097@naver.com>
2024-05-15 22:28:42 +0900

    Feat: 회원 서비스 구현

---
+++ src/main/kotlin/hello/hellospringkotlin/service/MemberService.kt
@@ -0,0 +1,48 @@
+package hello.hellospringkotlin.service
+
+import hello.hellospringkotlin.domain.Member
+import hello.hellospringkotlin.repository.MemoryMemberRepository
+
+class MemberService(private val memberRepository: MemoryMemberRepository) {
+
+    /**
+     * 회원 가입
+     */
+    fun join(member: Member): Long {
+        validateDuplicateMember(member)
+        memberRepository.save(member)
+        return member.id
+    }
+
+    private fun validateDuplicateMember(member: Member) {
+        memberRepository.findByName(member.name)?.let {
+            throw IllegalStateException("이미 존재하는 회원입니다.")
+        }
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    fun findMembers(): List<Member> {
+        return memberRepository.findAll()
+    }
+
+    fun findOne(memberId: Long): Member? {
+        return memberRepository.findById(memberId)
+    }
+
+    /**
+     * 회원 이름 변경
+     */
+    fun updateMemberName(id: Long, name: String): String? {
+        val member = memberRepository.update(id, name)
+        return member?.name
+    }
+
+    /**
+     * 회원 삭제
+     */
+    fun deleteMember(id: Long): List<Member> {
+        return memberRepository.delete(id)
+    }
+}
---
+++ src/test/kotlin/hello/hellospringkotlin/service/MemberServiceTest.kt
@@ -0,0 +1,20 @@
+package hello.hellospringkotlin.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MemberServiceTest {
+
+    @Test
+    fun join() {
+    }
+
+    @Test
+    fun findMembers() {
+    }
+
+    @Test
+    fun findOne() {
+    }
+}
