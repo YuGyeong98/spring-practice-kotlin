47c84fbb2e9e6cb57700635bc2a2344589e1f1cf
YuGyeong98 <kuk3097@naver.com>
2024-05-15 16:55:06 +0900

    Feat: hello-api get 구현

--- src/main/kotlin/hello/hellospringkotlin/controller/HelloController.kt
+++ src/main/kotlin/hello/hellospringkotlin/controller/HelloController.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class HelloController {
@@ -19,4 +20,20 @@ class HelloController {
         model.addAttribute("name", name)
         return "hello-template"
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    fun helloString(@RequestParam("name") name: String): String {
+        return "hello $name"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    fun helloApi(@RequestParam("name") name: String): Hello {
+        return Hello(name)
+    }
 }
+
+class Hello(
+    val name: String
+)
